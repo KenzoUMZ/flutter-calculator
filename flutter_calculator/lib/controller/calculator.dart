9cd38738e8af5edff29eb77efe32645d2bb89a0d
Cividati <rubens.cividati.teles@gmail.com>
2022-04-28 11:22:15 -0300

    feat: add return for method calculate

--- flutter_calculator/lib/controller/calculator.dart
+++ flutter_calculator/lib/controller/calculator.dart
@@ -1,11 +1,40 @@
 class Calculator {
-  double num1;
-  double num2;
+  String text;
+  double num1 = 0;
+  double num2 = 0;
+  String operation = '';
   // Operations methods
 
-  Calculator(this.num1, this.num2) {
-    num1 = num1;
-    num2 = num2;
+  Calculator(this.text) {
+    text = text;
+
+    // definir operação
+    String operation = '';
+    if (text.indexOf('+') != -1)
+      operation = '+';
+    else if (text.indexOf('-') != -1)
+      operation = '-';
+    else if (text.indexOf('*') != -1)
+      operation = '*';
+    else if (text.indexOf('/') != -1) operation = '/';
+
+    List<String> nums = text.split(operation);
+
+    num1 = double.parse(nums[0]);
+    num2 = double.parse(nums[1]);
+  }
+
+  double calculate() {
+    if (operation == '+')
+      return this.sum();
+    else if (operation == '-')
+      return this.sub();
+    else if (operation == '*')
+      return this.mul();
+    else if (operation == '/')
+      return this.div();
+    else
+      return 0;
   }
 
   double sum() {
