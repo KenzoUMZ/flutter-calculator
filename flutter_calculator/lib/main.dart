85474b65e2ffaec05e560737c539aa2acff5f661
Kenzo Umezawa <kenzoumezawa@gmail.com>
2022-04-28 13:42:59 -0300

    feat: Basic operations implemented

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calculator/view/components.dart';
+
 import 'view/home_screen.dart';
 
 void main() {
--- flutter_calculator/lib/view/home_screen.dart
+++ flutter_calculator/lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/controller/calculator.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,16 +10,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _operation = 0;
-
-  void _changeText(int t) {
+  String _operation = '';
+  String _result = '';
+  bool empty = false;
+  void _changeText(String t) {
     setState(() {
-      _operation = t;
+      _operation += t;
+      if (_operation.length == 5) {
+        _cleanDisplay();
+      }
+    });
+  }
+
+  void _cleanDisplay() {
+    setState(() {
+      _operation = '';
+      _result = '';
+    });
+  }
+
+  void _changeResult(String t) {
+    setState(() {
+      _result = t;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    TextStyle displayStyle =
+        GoogleFonts.montserrat(fontSize: 80, color: Colors.white);
     return Scaffold(
       body: Container(
           decoration: BoxDecoration(
@@ -30,10 +50,16 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Container(
                   padding:
-                      const EdgeInsets.only(bottom: 200, top: 80, left: 180),
+                      const EdgeInsets.only(bottom: 50, top: 20,),
                   child: Text(
-                    '$_operation',
-                    style: const TextStyle(color: Colors.white, fontSize: 80),
+                    _operation,
+                    style: displayStyle,
+                  textAlign: TextAlign.right,)),
+              Container(
+                  padding: const EdgeInsets.only( bottom: 50),
+                  child: Text(
+                    _result,
+                    style: displayStyle,
                   )),
               numpadBuilder()
             ],
@@ -48,6 +74,8 @@ class _HomeScreenState extends State<HomeScreen> {
     Color buttonTextColor = Colors.white;
     TextStyle keybStyle =
         GoogleFonts.baloo2(color: buttonTextColor, fontSize: 20);
+    Calculator calculator = Calculator();
+    GoogleFonts.baloo2(color: buttonTextColor, fontSize: 20);
     return Container(
         child: Expanded(
       child: Container(
@@ -60,127 +88,178 @@ class _HomeScreenState extends State<HomeScreen> {
                   begin: Alignment.bottomCenter,
                   end: Alignment.topCenter,
                   colors: <Color>[Colors.deepPurple.shade900, Colors.purple])),
-          child: Column(children: [
-            Row(
-              children: [
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '7',
-                        style: keybStyle,
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '8',
-                        style: keybStyle,
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '9',
-                        style: keybStyle,
-                      ),
-                    )),
-                SizedBox(
-                    width: 80,
-                    child: ElevatedButton(
-                      onPressed: () => {},
-                      child: Text('+'),
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.purple)),
-                    ))
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '4',
-                        style: keybStyle,
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '5',
-                        style: keybStyle,
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '6',
-                        style: keybStyle,
-                      ),
-                    )),
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '1',
-                        style: keybStyle,
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '2',
-                        style: keybStyle,
-                      ),
-                    )),
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: buttonDistX, vertical: buttonDistY),
-                    child: FloatingActionButton(
-                      backgroundColor: buttonColor,
-                      onPressed: () => {},
-                      child: Text(
-                        '3',
-                        style: keybStyle,
-                      ),
-                    ))
-              ],
-            )
+          child: Row(children: [
+            
+            Column(children: [FloatingActionButton(onPressed: ()=>{}),
+              Row(
+                children: [
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('7')},
+                        child: Text(
+                          '7',
+                          style: keybStyle,
+                        ),
+                      )),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('8')},
+                        child: Text(
+                          '8',
+                          style: keybStyle,
+                        ),
+                      )),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('9')},
+                        child: Text(
+                          '9',
+                          style: keybStyle,
+                        ),
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('4')},
+                        child: Text(
+                          '4',
+                          style: keybStyle,
+                        ),
+                      )),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('5')},
+                        child: Text(
+                          '5',
+                          style: keybStyle,
+                        ),
+                      )),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('6')},
+                        child: Text(
+                          '6',
+                          style: keybStyle,
+                        ),
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('1')},
+                        child: Text(
+                          '1',
+                          style: keybStyle,
+                        ),
+                      )),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('2')},
+                        child: Text(
+                          '2',
+                          style: keybStyle,
+                        ),
+                      )),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: buttonDistX, vertical: buttonDistY),
+                      child: FloatingActionButton(
+                        backgroundColor: buttonColor,
+                        onPressed: () => {_changeText('3')},
+                        child: Text(
+                          '3',
+                          style: keybStyle,
+                        ),
+                      )),
+                ],
+              )
+            ]),
+            Container(
+                padding: const EdgeInsets.only(left: 20, top: 10),
+                child: Column(
+                  children: [
+                    SizedBox(
+                        height: 150,
+                        width: 80,
+                        child: ElevatedButton(
+                          onPressed: () => {_changeText('+')},
+                          child: Text('+'),
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.purpleAccent),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ))),
+                        )),
+                    Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: SizedBox(
+                            height: 80,
+                            width: 80,
+                            child: ElevatedButton(
+                              onPressed: () => {_changeText('-')},
+                              child: Text('-'),
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Colors.purpleAccent),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ))),
+                            ))),
+                    SizedBox(
+                        width: 80,
+                        child: ElevatedButton(
+                          onPressed: () => {
+                            calculator.text = _operation,
+                            print(calculator.text),
+                            print(calculator.calculate()),
+                            _changeResult(calculator.calculate().toString()),
+                          },
+                          child: Text('='),
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.purple),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ))),
+                        ))
+                  ],
+                ))
           ])),
     ));
   }
