7f3dffea4cd5c6a73cf1b9aab43f98dace5e6dde
Samuel Kenzo Umezawa <kenzoumezawa@gmail.com>
2022-05-10 05:44:41 -0300

    fix: Status bar doens't hide the display

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-
 import 'view/home_screen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  // Add these 2 lines
+  WidgetsFlutterBinding.ensureInitialized();
+  
+ SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    
+ ));
   runApp(const MaterialApp(
     home: HomeScreen(),
   ));
 }
-
-
