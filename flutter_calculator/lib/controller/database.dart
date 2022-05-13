065139483a5a23850b0d84c62f1a7b5a61506e53
Cividati <rubens.cividati.teles@gmail.com>
2022-05-12 23:56:36 -0300

    feat: create, read, update and delete methods

--- flutter_calculator/lib/controller/database.dart
+++ flutter_calculator/lib/controller/database.dart
@@ -2,37 +2,61 @@ import 'dart:convert';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class Database {
-  String REMOTE_URL =
-      'mongodb+srv://cividatimongodb:Fyx2Mlk43rHaWsd3@cluster0.paiq2.mongodb.net/calculadora?retryWrites=true&w=majority';
-
   String LOCAL_URL = 'mongodb://localhost:27017/calculadora';
   var db;
+  bool mock = false;
 
-  Database({bool test = false}) {}
-
-  connect({bool test = false}) async {
-    if (test == true) {
-      print('Connecting to local db...');
-      this.db = await Db.create(LOCAL_URL);
-      db.open();
-    } else if (test == false) {
-      print('Connecting to remote db...');
-      this.db = await Db.create(REMOTE_URL);
-      db.open();
-    }
+  Database({bool isMock = false}) {
+    mock = isMock;
   }
 
-  changeVar() {
-    db = 'alterando o valor de db';
+  connect({bool isMock = false}) async {
+    db = await Db.create(LOCAL_URL);
+    await db.open();
   }
 
   create(var _json, String collection) async {
-    db = await Db.create(LOCAL_URL);
-    await db.open();
-    //connect(test: true);
-    var col = db.collection(collection);
-    await col.insertOne(_json);
-    //await col.insertOne(<String, dynamic>{'name': 'rubens'});
-    return true;
+    try {
+      await connect();
+      var coll = db.collection(collection);
+      await coll.insertOne(_json);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  read(var _json, String collection) async {
+    try {
+      await connect();
+      var coll = db.collection(collection);
+      var obj = await coll.findOne(where.eq('_id', _json['_id']));
+      return obj;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  update(var _json, String collection) async {
+    try {
+      await connect();
+      var coll = db.collection(collection);
+      // var obj = await coll.findOne(where.eq('_id', _json['id']));
+      await coll.save(_json);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  delete(var _json, String collection) async {
+    try {
+      await connect();
+      var coll = db.collection(collection);
+      await coll.remove({'_id': _json['_id']});
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
